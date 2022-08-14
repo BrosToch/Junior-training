b8cc39ede98a2adc4cf40f5317f3c69d0ebb878e
Bros Toch <brostoch0031@gmail.comm>
2022-08-14 10:37:56 +0700

    init project

---
+++ src/components/about.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h6>Hello from about page</h6>
+    </div>
+</template>
+<script>
+export default {
+    name: "about_page"
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,21 @@
+<template >
+    <div>
+        <h5 style="color:red">Hello from home page</h5>
+        <h5>This is home page</h5>
+        <about/>
+    </div>
+</template>
+<script>
+import about from '../components/about.vue'
+import About from '../components/about.vue';
+export default {
+    name: 'home_page',
+    components:{
+    about,
+    About
+}
+}
+</script>
+<style >
+    
+</style>
